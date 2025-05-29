ff5afc97a81df7b7fbc5d8d1a55803bc600f04b0
rohithDudam <rohith.dudam@intrinsics.com>
2025-05-29 14:35:44 +0530

    api for cases is done and also implemented CRUD

--- td.backend/index.js
+++ td.backend/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
+const casesRoutes = require('./routes/cases');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/cases', require('./routes/cases'));
+app.use('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the API' });
+});
 
 app.listen(5000, () => {
   console.log('Server running on port at: http://localhost:5000');
