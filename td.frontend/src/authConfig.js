ef2a6cb01a78ce14a96ac669db6d3e0b60749e00
omls-git <apoorva.sen@orcimedlifesciences.com>
2025-06-05 14:59:31 +0530

    authentication done

---
+++ td.frontend/src/authConfig.js
@@ -0,0 +1,28 @@
+export const msalConfig = {
+  auth: {
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}`, // Or use 'common' for multi-tenant
+    redirectUri: "http://localhost:3001", // Match this with Azure portal setting
+  },
+  cache: {
+    cacheLocation: "sessionStorage", // or 'sessionStorage'
+    storeAuthStateInCookie: false,
+  },
+};
+
+export const protectedResources = {
+  apiList: {
+    endpoint: "http://localhost:3001",
+    scopes: {
+      read: ["https://orcimedlifesciences.sharepoint.com"],
+      write: ["https://orcimedlifesciences.sharepoint.com"],
+    },
+  },
+};
+
+export const loginRequest = {
+  scopes: [
+    "User.Read",
+    "Sites.Read.All",
+  ],
+};
--- td.frontend/src/index.js
+++ td.frontend/src/index.js
@@ -3,15 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const msalInstance = new PublicClientApplication(msalConfig);
 
+async function main() {
+  try {
+    await msalInstance.initialize(); // ✅ REQUIRED
+    await msalInstance.handleRedirectPromise(); // ✅ REQUIRED for loginRedirect()
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+      <React.StrictMode>
+        <MsalProvider instance={msalInstance}>
+          <App />
+        </MsalProvider>
+      </React.StrictMode>
+    );
+  } catch (error) {
+    console.error('MSAL Initialization error:', error);
+  }
+}
+main();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
